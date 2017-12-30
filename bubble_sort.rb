681fd93334a80d612aec3cb2ed7ac561e396c86b
Ivan Szebenszki <szib@users.noreply.github.com>
2017-12-30 18:19:30 +0000

    Beautified

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,26 +2,26 @@ def bubble_sort(arr)
   l = arr.length
   while l != 0
     newl = 0
-    1.upto(l-1) { |i| arr[i], arr[i-1], newl = arr[i-1], arr[i], i if arr[i-1] > arr[i] }
+    1.upto(l - 1) { |i| arr[i], arr[i - 1], newl = arr[i - 1], arr[i], i if arr[i - 1] > arr[i] }
     l = newl
   end
   arr
 end
 
-def bubble_sort_by(arr,&block)
+def bubble_sort_by(arr)
   l = arr.length
   while l != 0
     newl = 0
-    1.upto(l-1) { |i| arr[i], arr[i-1], newl = arr[i-1], arr[i], i if yield(arr[i-1], arr[i]) > 0 }
+    1.upto(l - 1) { |i| arr[i], arr[i - 1], newl = arr[i - 1], arr[i], i if yield(arr[i - 1], arr[i]) > 0 }
     l = newl
   end
   arr
 end
 
-puts bubble_sort([4,5,6,2,23,4,5,23,24,1,-3,14,5,623,3]).inspect
+puts bubble_sort([4, 5, 6, 2, 23, 4, 5, 23, 24, 1, -3, 14, 5, 623, 3]).inspect
 puts bubble_sort([]).inspect
-puts bubble_sort([1,-1]).inspect
+puts bubble_sort([1, -1]).inspect
 puts bubble_sort([1]).inspect
 
-puts bubble_sort_by(["gdfadg", "lksgf", "sf", "s"]) { |l,r| l.length - r.length }.inspect
-puts bubble_sort_by([4,5,6,2,23,4,5,23,24,1,-3,14,5,623,3]) { |l,r| l.abs%10 - r.abs%10 }.inspect
+puts bubble_sort_by(%w[gdfadg lksgf sf s]) { |l, r| l.length - r.length }.inspect
+puts bubble_sort_by([4, 5, 6, 2, 23, 4, 5, 23, 24, 1, -3, 14, 5, 623, 3]) { |l, r| l.abs % 10 - r.abs % 10 }.inspect
